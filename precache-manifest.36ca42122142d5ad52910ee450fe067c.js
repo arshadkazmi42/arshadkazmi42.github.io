e12e2076d7cc10938e03739183e9c1b1032e4d96
Arshad Kazmi <arshadkazmi42@gmail.com>
2018-10-11 23:06:03 +0530

    Updates

---
+++ precache-manifest.36ca42122142d5ad52910ee450fe067c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e860ce1630f76144d629",
+    "url": "/static/js/main.e860ce16.chunk.js"
+  },
+  {
+    "revision": "6ab9d7527bbbb9d6fe3b",
+    "url": "/static/js/1.6ab9d752.chunk.js"
+  },
+  {
+    "revision": "e860ce1630f76144d629",
+    "url": "/static/css/main.ad2d5fd3.chunk.css"
+  },
+  {
+    "revision": "6a52da5d6d94c2e0eca38b401d0d6272",
+    "url": "/index.html"
+  }
+];
